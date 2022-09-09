cad08b8dd77701a8a2c629eae0a59e7a11b9959d
Vlad Shchelkunov <specter.ode@gmail.com>
2022-09-09 21:28:58 +0300

    test for start

--- src/components/Header/UserMenu/UserMenu.jsx
+++ src/components/Header/UserMenu/UserMenu.jsx
@@ -11,6 +11,7 @@ const getActive = ({ isActive }) => {
 
 const UserMenu = () => {
   const auth = useSelector(state => state.auth);
+  console.log('auth: ', auth);
   const dispatch = useDispatch();
   const exit = () => {
     dispatch(logout());
@@ -18,27 +19,31 @@ const UserMenu = () => {
 
   return (
     <nav className={s.nav}>
-      <div className={s.thumb}>
-        <NavLink className={getActive} to="/">
-          Home
-        </NavLink>
-        {auth.isLoggedIn && (
+      <ul className={s.list}>
+        <li className={s.item}>
+          <NavLink className={getActive} to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
           <NavLink className={getActive} to="/contacts">
             Phonebook
           </NavLink>
-        )}
-      </div>
+        </li>
+      </ul>
       <div className={s.thumb}>
-        {auth.isLoggedIn && (
-          <>
-            <span className={s.title}>
-              Welcome, <span className={s.name}>{auth.user.name}</span>
+        <>
+          <span className={s.title}>
+            Welcome,
+            <span className={s.name}>
+              {auth?.user?.name}
+              Имя пользователя
             </span>
-            <button className={s.button} type="button" onClick={exit}>
-              Exit
-            </button>
-          </>
-        )}
+          </span>
+          <button className={s.button} type="button" onClick={exit}>
+            Exit
+          </button>
+        </>
       </div>
     </nav>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,76 @@
-// import s from './HomePage.module.css'
+import s from './HomePage.module.css';
 import React from 'react';
-
+import UserForm from 'components/UserForm/UserForm';
+import {
+  useGetIncomeCategoriesQuery,
+  useGetExpenseCategoriesQuery,
+  useGetPeriodDataQuery,
+  useGetIncomeQuery,
+  useGetExpenseQuery,
+  useDeleteTransactionMutation,
+  useAddExpenseMutation,
+  useAddIncomeMutation,
+} from 'redux/transaction/transactionOperations';
+import {
+  useGetUserQuery,
+  useUpdateBalanceMutation,
+} from 'redux/user/userOperations';
+import { useState } from 'react';
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [expense, setExpense] = useState({
+    description: 'Income description',
+    amount: 100,
+    date: '2020-12-31',
+    category: 'Доп. доход',
+  });
+  // const { data, isLoading, error } = useGetIncomeCategoriesQuery();
+  // const {data, isLoading, error} = useGetExpenseCategoriesQuery();
+  // const {data, isLoading, error} = useGetPeriodDataQuery('2022-06');
+  // const {data, isLoading, error} = useGetIncomeQuery();
+  // const {data, isLoading, error} = useGetExpenseQuery();
+  // const { data, isLoading, error } = useGetUserQuery();
+
+  const [deleteTransaction, data1] = useDeleteTransactionMutation();
+  const [addExpense, data2] = useAddExpenseMutation();
+  const [addIncome, data3] = useAddIncomeMutation();
+  const [updateBalance, data4] = useUpdateBalanceMutation();
+
+  return (
+    <div className={s.container}>
+      <h2>HomePage</h2>
+      <UserForm />
+      <div className={s.block}>
+        <button
+          className={s.item}
+          type="button"
+          onClick={() => deleteTransaction({ newBalance: 111 })}
+        >
+          detele transaction
+        </button>
+        <button
+          className={s.item}
+          type="button"
+          onClick={() => addExpense({ newBalance: 111 })}
+        >
+          add expense
+        </button>
+        <button
+          className={s.item}
+          type="button"
+          onClick={() => addIncome({ newBalance: 111 })}
+        >
+          Add income
+        </button>
+        <button
+          className={s.item}
+          type="button"
+          onClick={() => updateBalance({ newBalance: 111 })}
+        >
+          update Balance
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
