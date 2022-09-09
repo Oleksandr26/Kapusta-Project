77228487bec47782c846b578bad4388220ceb60d
Maksym Pokulytyi <mlightpro@gmail.com>
2022-09-09 22:41:20 +0300

    add svg

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import s from './HomePage.module.css';
+import Alkohol from 'assets/svg/alkohol.svg';
 import React from 'react';
 import UserForm from 'components/UserForm/UserForm';
 import {
@@ -16,6 +17,8 @@ import {
   useUpdateBalanceMutation,
 } from 'redux/user/userOperations';
 import { useState } from 'react';
+import Balance from 'components/Balance/Balance';
+import { Link } from 'react-router-dom';
 const HomePage = () => {
   const [expense, setExpense] = useState({
     description: 'Income description',
@@ -38,6 +41,12 @@ const HomePage = () => {
   return (
     <div className={s.container}>
       <h2>HomePage</h2>
+      <div>
+        <Balance />
+        <Link>
+          Reports <Alkohol size="15px" />
+        </Link>
+      </div>
       <UserForm />
       <div className={s.block}>
         <button
--- src/pages/ReportsPage/ReportsPage.jsx
+++ src/pages/ReportsPage/ReportsPage.jsx
@@ -1,12 +1,15 @@
 import s from './ReportsPage.module.css';
 import React from 'react';
+import Balance from 'components/Balance/Balance';
 
 const ReportPage = () => {
   return (
     <>
       <div className={s.header}>
         <button className={s.item}>Main menu</button>
-        <div className={s.item}>Balance</div>
+        <div className={s.item}>
+          <Balance />
+        </div>
         <div className={s.item}>Calendar</div>
       </div>
     </>
