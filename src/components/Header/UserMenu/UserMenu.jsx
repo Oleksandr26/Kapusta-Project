bc3f8e9f100206ca2eb5acb67c86b13224f5ff23
Sem Kholodov <kholodovsem@gmail.com>
2022-09-11 11:51:10 +0300

    Update UserMenu.jsx

--- src/components/Header/UserMenu/UserMenu.jsx
+++ src/components/Header/UserMenu/UserMenu.jsx
@@ -6,18 +6,16 @@ import { logout } from 'redux/auth/auth-operations';
 import { ReactComponent as LogOut } from '../../../assets/svg/logout.svg';
 
 const UserMenu = () => {
-  const userData = useSelector(state => state.auth.userData);
+  const email = useSelector(state => state.auth.email);
   const dispatch = useDispatch();
   const exit = () => {
     dispatch(logout());
   };
-  // console.log(userData.email);
-  const user = userData.email;
 
   return (
-    user && (
+    email && (
       <nav className={s.nav}>
-        <span className={s.name}>{user[0].toUpperCase()}</span>
+        <span className={s.name}>{email[0].toUpperCase()}</span>
 
         <LogOut className={s.icon_logOut} onClick={exit} />
       </nav>
