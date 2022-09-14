4bcb4569bef77c3786c9f2705fbe04e95d92048f
Oleksandr26 <alemaevaleksandr96@gmail.com>
2022-09-14 22:20:34 +0300

    added new style

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,30 +1,36 @@
 import s from './Dashboard.module.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { TransactionDetailsMobile } from './TransactionDetails/TransactionsDetailsMobile/TransactionsDetailsMobile';
 
 const getLinkClassName = ({ isActive }) => (isActive ? s.activeLink : s.link);
-
 const Dashboard = () => {
+  const width = window.innerWidth;
+  console.log('width: ', width);
+  const location = useLocation();
+  const bool = location.pathname === '/transactions' || width > 768;
+  console.log('bool: ', bool);
   return (
     <div className={s.container}>
-      <TransactionDetailsMobile />
-      <ul className={s.wrapper_nav}>
-        <li className={s.item}>
-          <NavLink className={getLinkClassName} to="expenses">
-            Expenses
-          </NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink className={getLinkClassName} to="income">
-            Income
-          </NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink className={(getLinkClassName, s.summ__link)} to="summary">
-            Summary
-          </NavLink>
-        </li>
-      </ul>
+      {location.pathname === '/transactions' && <TransactionDetailsMobile />}
+      {bool && (
+        <ul className={s.wrapper_nav}>
+          <li className={s.item}>
+            <NavLink className={getLinkClassName} to="expenses">
+              Expenses
+            </NavLink>
+          </li>
+          <li className={s.item}>
+            <NavLink className={getLinkClassName} to="income">
+              Income
+            </NavLink>
+          </li>
+          <li className={s.item}>
+            <NavLink className={(getLinkClassName, s.summ__link)} to="summary">
+              Summary
+            </NavLink>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
