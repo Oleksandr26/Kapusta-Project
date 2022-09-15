a0d22ea6f876838d327dcd80a9b87d7dbf8c0205
Maksym Pokulytyi <mlightpro@gmail.com>
2022-09-15 10:26:46 +0300

    clear not used import

--- src/components/Dashboard/ExpensesAndIncome/Button.jsx
+++ src/components/Dashboard/ExpensesAndIncome/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink /*, useLocation*/ } from 'react-router-dom';
 
 import s from './ExpensesAndIncome.module.css';
 
@@ -19,26 +19,25 @@ export const Button = ({ onClickSubmit, onClickReset }) => {
 
 const getLinkClassName = ({ isActive }) => (isActive ? s.activeLink : s.link);
 export const ButtonTransactions = () => {
-  const { pathname } = useLocation();
-  const showButtons = pathname === '/transactions' && window.innerWidth < 768;
-  console.log('showButtons: ', showButtons);
+  // const { pathname } = useLocation();
+  // const showButtons =
+  //   pathname === '/transactions/expenses' && window.innerWidth > 768;
   return (
     <>
-      {' '}
-      {showButtons && (
-        <ul className={s.wrapper_nav}>
-          <li className={s.item}>
-            <NavLink className={getLinkClassName} to="/transactions/expenses">
-              Expenses
-            </NavLink>
-          </li>
-          <li className={s.item}>
-            <NavLink className={getLinkClassName} to="/transactions/incomes">
-              Income
-            </NavLink>
-          </li>
-        </ul>
-      )}
+      {/* {showButtons && ( */}
+      <ul className={s.wrapper_nav}>
+        <li className={s.item}>
+          <NavLink className={getLinkClassName} to="/transactions/expenses">
+            Expenses
+          </NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink className={getLinkClassName} to="/transactions/incomes">
+            Income
+          </NavLink>
+        </li>
+      </ul>
+      {/* )} */}
     </>
   );
 };
