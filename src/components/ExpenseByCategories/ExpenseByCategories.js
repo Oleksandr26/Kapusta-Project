f6d79fdb4fb53bffd8cfd2286568bc673ce86c71
Maksym Pokulytyi <mlightpro@gmail.com>
2022-09-19 14:19:16 +0300

    add transliteration to categories

--- src/components/ExpenseByCategories/ExpenseByCategories.js
+++ src/components/ExpenseByCategories/ExpenseByCategories.js
@@ -9,8 +9,14 @@ import {
   useGetExpenseQuery,
 } from 'redux/transaction/transactionOperations';
 import { NavLink } from 'react-router-dom';
+import categoriesData from './categoriesData.json';
 
-const getLinkClassName = ({ isActive }) => (isActive ? s.activeLink : s.link);
+// const key = Object.keys(categoriesData);
+
+const getLinkClassName = ({ isActive }) => {
+  console.log('isActive: ', isActive);
+  return isActive ? s.activeLink : s.link;
+};
 
 const ExpenseByCategories = ({
   dateTransactionFilter,
@@ -34,48 +40,52 @@ const ExpenseByCategories = ({
         dateTransactionFilter(expenses)?.find(
           expense => expense.category === item
         )?._id ?? '',
+      convertName: categoriesData[item],
     };
   });
 
-  const elements = result?.map(({ name, amount, id }) => {
-    const iconPath = sprite + `#${name}`;
-    const backgroundPath = backgroundSprite + `#${name}`;
-    const amountNormalizer = amount
-      .toFixed(2)
-      .replace(/\d(?=(\d{3})+\.)/g, '$& ');
+  console.log('result: ', result);
+  const elements = result
+    ?.filter(({ amount }) => amount > 0)
+    ?.map(({ name, amount, id, convertName }) => {
+      const iconPath = sprite + `#${name}`;
+      const backgroundPath = backgroundSprite + `#${name}`;
+      const amountNormalizer = amount
+        .toFixed(2)
+        .replace(/\d(?=(\d{3})+\.)/g, '$& ');
 
-    const handleSetCategory = () => {
-      if (category === name) {
-        return;
-      }
-      setCategory(name);
-    };
-    return (
-      <li className={s.item} key={nanoid()} onClick={handleSetCategory}>
-        <p className={s.info}>{amountNormalizer}</p>
-        {id ? (
-          <NavLink to={id} className={getLinkClassName}>
-            <svg className={s.iconBackground} width="56px" height="56px">
-              <use href={backgroundPath}></use>
-            </svg>
-            <svg className={s.icon} width="56px" height="56px">
-              <use href={iconPath}></use>
-            </svg>
-          </NavLink>
-        ) : (
-          <div className={s.link}>
-            <svg className={s.iconBackground} width="56px" height="56px">
-              <use href={backgroundPath}></use>
-            </svg>
-            <svg className={s.icon} width="56px" height="56px">
-              <use href={iconPath}></use>
-            </svg>
-          </div>
-        )}
-        <p className={s.info}>{name}</p>
-      </li>
-    );
-  });
+      const handleSetCategory = () => {
+        if (category === name) {
+          return;
+        }
+        setCategory(name);
+      };
+      return (
+        <li className={s.item} key={nanoid()} onClick={handleSetCategory}>
+          <p className={s.info}>{amountNormalizer}</p>
+          {id ? (
+            <NavLink to={convertName} className={getLinkClassName}>
+              <svg className={s.iconBackground} width="56px" height="56px">
+                <use href={backgroundPath}></use>
+              </svg>
+              <svg className={s.icon} width="56px" height="56px">
+                <use href={iconPath}></use>
+              </svg>
+            </NavLink>
+          ) : (
+            <div className={s.link}>
+              <svg className={s.iconBackground} width="56px" height="56px">
+                <use href={backgroundPath}></use>
+              </svg>
+              <svg className={s.icon} width="56px" height="56px">
+                <use href={iconPath}></use>
+              </svg>
+            </div>
+          )}
+          <p className={s.info}>{name}</p>
+        </li>
+      );
+    });
   return (
     <>
       {isFetching ? (
