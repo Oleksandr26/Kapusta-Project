f7e311db8c63588fc06dd19c2d88f5f9266ec04c
Vlad Shchelkunov <specter.ode@gmail.com>
2022-09-22 16:52:26 +0300

    styles for chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -12,14 +12,11 @@ const Chart = ({ dateTransactionFilter, category }) => {
   const incomes = useGetIncomeQuery().currentData?.incomes;
   const MONTH_CASHFLOW = [];
   const [diagramWidth, setDiagramWidth] = useState(window.innerWidth - 40);
-  //   let ww = window.innerWidth - 40;
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const handleResize = () => {
-    // console.log('handleResize: ');
     if (window.innerWidth < 480) {
       setDiagramWidth(window.innerWidth - 40);
     } else setDiagramWidth(440);
-    // console.log('diagramWidth: ', diagramWidth);
     setIsMobile(window.innerWidth < 768);
   };
   useEffect(() => {
@@ -41,38 +38,46 @@ const Chart = ({ dateTransactionFilter, category }) => {
     }))
     .sort((firstAmount, secondAmount) => secondAmount.amount - firstAmount.amount)
     .filter(el => el.amount !== 0);
-  const maxPrice = diagramForSelectedMonth[0]?.amount;
-  //   const diagramWidthForMobile = window.innerWidth < 480 ? window.innerWidth - 40 : 440;
-  //   console.log('innerWidth: ', window.innerWidth);
-  //   console.log('diagramWidthForMobile: ', diagramWidthForMobile);
+  const maxAmount = diagramForSelectedMonth[0]?.amount;
   return (
     <ul className={s.list}>
       {diagramForSelectedMonth.map(({ descriptionName, amount }, index) => (
-        <li style={{ width: '10%' }} key={nanoid()}>
-          <div className={s.description}>
-            <p className={s.category}>{descriptionName}&nbsp;</p>
-            <p className={s.price}>{formated(amount)}&nbsp;&#8372;</p>
-          </div>
+        <li className={s.item} key={nanoid()}>
           {isMobile ? (
-            <div
-              style={{
-                width: (amount / maxPrice) * diagramWidth,
-                height: 15,
-                background: index % 3 === 0 ? '#FF751D' : '#FED9BF',
-                borderTopRightRadius: 10,
-                borderBottomRightRadius: 10,
-              }}
-            ></div>
+            <>
+              <div
+                className={s.description}
+                style={{
+                  width: (amount / maxAmount) * diagramWidth + 10,
+                }}
+              >
+                <p className={s.category}>{descriptionName}&nbsp;</p>
+                <p className={s.price}>{formated(amount)}&nbsp;&#8372;</p>
+              </div>
+              <div
+                style={{
+                  width: (amount / maxAmount) * diagramWidth,
+                  height: 15,
+                  background: index % 3 === 0 ? '#FF751D' : '#FED9BF',
+                  borderTopRightRadius: 10,
+                  borderBottomRightRadius: 10,
+                }}
+              ></div>
+            </>
           ) : (
-            <div
-              style={{
-                width: 38,
-                height: (amount / maxPrice) * 328,
-                background: index % 3 === 0 ? '#FF751D' : '#FED9BF',
-                borderTopRightRadius: 10,
-                borderTopLeftRadius: 10,
-              }}
-            ></div>
+            <>
+              <p className={s.price}>{formated(amount)}&nbsp;&#8372;</p>
+              <div
+                style={{
+                  width: 38,
+                  height: (amount / maxAmount) * 328,
+                  background: index % 3 === 0 ? '#FF751D' : '#FED9BF',
+                  borderTopRightRadius: 10,
+                  borderTopLeftRadius: 10,
+                }}
+              ></div>
+              <p className={s.category}>{descriptionName}&nbsp;</p>
+            </>
           )}
         </li>
       ))}
